472fdd1f85cfca2e3cabe96cf140669bc04655f2
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-07-29 23:07:00 -0300

    validationBudget

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+import { StyleSheet, View, Alert  } from 'react-native';
 import { Header } from './src/component/Header';
 import NewBudget from './src/component/NewBudget';
 
 export default function App() {
+
+  const handleBudget=(budget)=>{
+    if(Number(budget) > 0){
+      console.log('todo bien')
+    }else{
+      Alert.alert('Error', 'El presupuesto no puede ser 0 o menor a 0', 'OK')
+    }
+  }
+
+
   return (
     <View style={styles.container}>
       <View>
         <Header style={styles.header}></Header>
-        <NewBudget></NewBudget>
+        <NewBudget 
+          handleBudget={handleBudget}
+        ></NewBudget>
       </View>
     </View>
   );
--- src/component/NewBudget.js
+++ src/component/NewBudget.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import { useState } from 'react';
 import { View, Text, Pressable, TextInput } from 'react-native'
 import styles from '../Styles/NewBudget.jsx'
-const NewBudget = () => {
+const NewBudget = ({handleBudget, value}) => {
+
+  const [budget, setBudget] = useState(0);
+
   return (
     <View style={styles.container}>
       <Text  style={styles.label}>Definir Presupuesto</Text>
       <TextInput
         keyboardType='numeric'
         placeholder='Agregar presupuesto'
+        value={budget.toString()}
+        onChangeText={setBudget}
         style={styles.input}
       />
-      <Pressable  style={styles.button}>
+      <Pressable  
+        style={styles.button}
+        onPress={()=>
+          handleBudget(budget)
+        }
+        >
         <Text  style={styles.buttonText}>Agregar presupuesto</Text>
       </Pressable>
     </View>
