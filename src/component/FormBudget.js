d81053cfdf592d7ceab6f2e54c726687e2c0bad7
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-08-15 18:59:57 -0300

    newInput

--- src/component/FormBudget.js
+++ src/component/FormBudget.js
@@ -14,11 +14,20 @@ const FormBudget = () => {
             </View>
             <View style={form}>
                 <Text style={title}>New Bills</Text>
+
                 <View style={styles.containerInput}>
                     <Text style={styles.laber}>Expense name</Text>
                     <TextInput
                         style={styles.input}
                         placeholder='Expense name. ej. Cine'
+                    />
+                </View>
+
+                <View style={styles.containerInput}>
+                    <Text style={styles.laber}>Amount spent</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder='Spending amount. ej. 700'
                     />
                 </View>
             </View>
