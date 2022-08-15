fea63560e158ab2b80245798d7350f83d593c806
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-08-15 15:34:16 -0300

    importStyles

--- src/component/FormBudget.js
+++ src/component/FormBudget.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-
 import { View, Text, Pressable, TextInput } from 'react-native'
+import styles from '../Styles/FormBudget.jsx'
+
 const FormBudget = () => {
     return (
         <View>
