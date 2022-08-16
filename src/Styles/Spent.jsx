33330ceb3494c394a2f0a3fe47a53e943457d679
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-08-16 20:37:46 -0300

    containerStyles

--- src/Styles/Spent.jsx
+++ src/Styles/Spent.jsx
@@ -2,7 +2,20 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container:{
-        marginTop:70,
+        backgroundColor: 'white',
+        marginHorizontal: 30,
+        marginVertical: 10,
+        padding: 10,
+        borderRadius: 5,
+        transform: [{ translateY: 50 }],
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+        elevation: 3,
     },
     title:{
         fontSize:30,
