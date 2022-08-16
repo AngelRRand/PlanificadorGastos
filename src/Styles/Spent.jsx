05909f16ed73282bf3144ea3045efbf809ca98af
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-08-16 20:35:43 -0300

    stylesSpent

---
+++ src/Styles/Spent.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container:{
+        marginTop:70,
+    },
+    title:{
+        fontSize:30,
+        textAlign:'center',
+        fontWeight:700,
+    },
+    TextNotExp:{
+        marginTop:20,
+        textAlign:'center',
+        fontSize:20
+    }
+})
+
+export default styles
