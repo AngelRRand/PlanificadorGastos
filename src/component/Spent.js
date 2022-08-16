406c5910f0655b7da33f01afa32c0d20afad7a87
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-08-16 20:46:03 -0300

    estructureSpent

--- src/component/Spent.js
+++ src/component/Spent.js
@@ -2,13 +2,24 @@ import React from 'react'
 import { View, Text, Pressable, Image } from 'react-native'
 import styles from '../Styles/Spent.jsx'
 
-const Spent = ({bill}) => {
+const Spent = ({ bill }) => {
 
-  const {name, category, amount, id} = bill
+  const { name, category, amount, id } = bill
 
   return (
-    <View>
-        <Text>{name}</Text>
+    <View style={styles.container}>
+      <View>
+
+        <View>
+          
+          <Text>{category}</Text>
+          <Text>{name}</Text>
+
+        </View>
+        <Text>{amount}</Text>
+
+
+      </View>
     </View>
   )
 }
