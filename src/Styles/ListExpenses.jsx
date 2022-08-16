5a1f4b30eac5f8a10a32e39a738323ec8c6192aa
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-08-16 20:42:50 -0300

    fixedStylesALL

--- src/Styles/ListExpenses.jsx
+++ src/Styles/ListExpenses.jsx
@@ -2,15 +2,16 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container:{
-        marginTop:70,
+        marginVertical:90,
     },
     title:{
         fontSize:30,
         textAlign:'center',
         fontWeight:700,
+        marginTop:20
     },
     TextNotExp:{
-        marginTop:20,
+        marginVertical:20,
         textAlign:'center',
         fontSize:20
     }
