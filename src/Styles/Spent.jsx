730ad531b32e317550700e7b781728dbe8421079
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-08-16 21:13:15 -0300

    styles

--- src/Styles/Spent.jsx
+++ src/Styles/Spent.jsx
@@ -19,22 +19,34 @@ const styles = StyleSheet.create({
         marginBottom:20
     },
     containerCount:{
-
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center'
     },
     containerImage:{
-
+        flexDirection:'row',
+        alignItems:'center',
+        flex:1,
     },
     image:{
-
+        width: 80,
+        height: 80,
+        marginRight:20
     },
     containerText:{
-
+        flex:1,
     },
     category:{
-
+        color:'#94a3b8',
+        fontSize:16,
+        fontWeight:'700',
+        textTransform:'uppercase',
+        marginBottom:5,
     },
     name:{
-        
+        fontSize:22,
+        color:'#6c6f74',
+        marginBottom:5,
     }
 })
 
