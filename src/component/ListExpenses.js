321eba26a61410cc174f736e137ab7cdae4b1574
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-08-17 21:16:02 -0300

    fixedComponentList

--- src/component/ListExpenses.js
+++ src/component/ListExpenses.js
@@ -1,24 +1,35 @@
 import React from 'react'
 import { View, Text, Pressable, TextInput } from 'react-native'
 import Spent from './Spent'
-const ListExpenses = ({bills, setModal, setSpent}) => {
+const ListExpenses = ({ bills, setModal, setSpent, filtro, billsFilter }) => {
   return (
     <View style={styles.container}>
-        <Text style={styles.title}>Expenses</Text>
-        {
-            bills.length === 0 
-            ? 
-            <Text style={styles.TextNotExp}>No hay gastos..</Text>
-            :
-            bills.map(b => (
-                <Spent
-                    key={b.id}
-                    bill={b}
-                    setModal={setModal}
-                    setSpent={setSpent}
-                />
-            ))
-        }
+      <Text style={styles.title}>Expenses</Text>
+
+      {
+        filtro ? billsFilter.map(b => (
+          <Spent
+            key={b.id}
+            bill={b}
+            setModal={setModal}
+            setSpent={setSpent}
+          />
+        ))
+          :
+          bills.map(b => (
+            <Spent
+              key={b.id}
+              bill={b}
+              setModal={setModal}
+              setSpent={setSpent}
+            />
+          ))
+      }
+      {
+        bills.length === 0 || (billsFilter.length === 0 && !!filtro)&& (
+          <Text>No hay Gastos</Text>
+        )
+      }
     </View>
   )
 }
